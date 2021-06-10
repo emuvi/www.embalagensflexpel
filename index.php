420e45ee5867f358bf6ce07667ff8ef7f63144ad
Éverton M. Vieira <everton.muvi@gmail.com>
2021-06-10 14:13:17 -0300

    prototype development

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Flexpel Embalagens</title>
+  <link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+
+<body>
+  <div style="width: 900px; margin: auto;">
+    <p style="text-align: center"><img src="imgs/logo.jpg" /></p>
+    <div id="esquerda">
+      <h1>Flexpel Embalagens</h1>
+      <p>A Flexpel é uma empresa especializada no ramo de embalagens atuando no mercado a
+        mais de 15 anos. Fornecemos soluções em embalagens descartáveis para qualquer ramo
+        de atividade empresarial.</p>
+      <h2>Entre em Contato</h2>
+      <h3>Telefones</h3>
+      <p>(48) 98452-7036 (What's App)</p>
+      <p>(48) 99695-4174 (Tim)</p>
+      <p>(48) 98492-0354 (Oi)</p>
+      <p>(48) 3286-6583 (Fixo)</p>
+
+      <h3>E-Mails</h3>
+      <p><a
+          href="mailto:vendas@embalagensflexpel.com.br">vendas@embalagensflexpel.com.br</a>
+      </p>
+      <p><a
+          href="mailto:flexpel.embalagens@hotmail.com">flexpel.embalagens@hotmail.com</a>
+      </p>
+
+      <h3>Endereço</h3>
+      <p>Em Frente ao Shopping Continente</p>
+      <p>R. Capital, 220 - Fazenda Santo Antônio - São José - SC - CEP: 88.104-596</p>
+    </div>
+
+    <div id="direita">
+      <h2>Embalagens que Fornecemos</h2>
+    </div>
+  </div>
+  <br>
+</body>
+
+</html>
